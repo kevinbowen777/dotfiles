6c657cce64ad0f7a539182604b21dc6533fe6864
kevinbowen777 <kevin.bowen@gmail.com>
2021-05-26 20:19:19 -0700

    Add document templates

---
+++ templates/perl_boilerplate.pl
@@ -0,0 +1,21 @@
+#!/usr/bin/perl
+
+# {{{ --------------------------------------------------------------------- #
+#
+# Name: Name_of_Script.pl
+# Purpose: This script does ...X,Y, and Z.
+# Compatible:
+# Requirements:
+#
+# Version: 1.0
+# Author: Kevin Bowen kevin.bowen@gmail.com
+# 
+# Original source:
+# Updated: 2019111x
+#
+# }}} --------------------------------------------------------------------- #
+
+use strict;
+use warnings;
+use diagnostics;
+use 5.014;
